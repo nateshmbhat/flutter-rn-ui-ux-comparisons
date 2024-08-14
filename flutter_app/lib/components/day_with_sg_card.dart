629d28deacaaee98ecf12709d855ba70b539b5e7
nateshmbhat <nateshmbhat1@gmail.com>
2024-08-14 08:15:59 +0530

    adds sun anim & confetti

--- flutter_app/lib/components/day_with_sg_card.dart
+++ flutter_app/lib/components/day_with_sg_card.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-
-import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 
 const sunImage = 'lib/assets/img_sun.png';
 
